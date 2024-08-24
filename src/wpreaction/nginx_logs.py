b47221ef4620520fc65c38795cfee3666e954f6b
neko <neko@127.0.0.1>
2024-08-24 04:17:40 -0600

    log error instead of crashing

--- src/wpreaction/nginx_logs.py
+++ src/wpreaction/nginx_logs.py
@@ -21,10 +21,14 @@ def nginx_logs(filename, ban_cloud_providers, ban_xmlrpc_users, ban_rest_route_u
             if(ban_rest_route_users == 'True' and 'rest_route' in path): ban_ip('nginx_logs', toban)
 
             if(ban_cloud_providers == 'True'):
-                lookup = IPWhois(toban).lookup_whois()
-                name = lookup['nets'][0]['name'].lower()
+                try:
+                    lookup = IPWhois(toban).lookup_whois()
+                    name = lookup['nets'][0]['name'].lower()
 
-                if('digitalocean' in name or 'amazon' in name or 'microsoft' in name or 'dreamhost' in name or 'linode' in name or 'ovh' in name): ban_ip('nginx_logs', toban)
+                    if('digitalocean' in name or 'amazon' in name or 'microsoft' in name or 'dreamhost' in name or 'linode' in name or 'ovh' in name): ban_ip('nginx_logs', toban)
+
+                except Error as e:
+                    log.error(e)
 
         time.sleep(1)
 
