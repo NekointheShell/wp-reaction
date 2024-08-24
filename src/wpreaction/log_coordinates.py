a7004c279a5fbf929dad26b3201caa5b5dbbe437
neko <neko@127.0.0.1>
2024-08-24 12:54:15 -0600

    debug log coordinates

--- src/wpreaction/log_coordinates.py
+++ src/wpreaction/log_coordinates.py
@@ -24,8 +24,10 @@ def log_coordinates(filename, north_limit, south_limit, east_limit, west_limit):
             if(lookup != None):
                 coordinates = lookup.location
 
-                if(coordinates[0] <= north_limit and coordinates[0] >= south_limit and coordinates[1] >= east_limit and coordinates[1] <= west_limit):
-                    log.info('IP from coordinate limit: {}'.format(ip))
+                if(len(coordinates) == 2):
+                    log.debug('{} has coordinates {}'.format(ip, coordinates))
+                    if(coordinates[0] <= north_limit and coordinates[0] >= south_limit and coordinates[1] >= east_limit and coordinates[1] <= west_limit):
+                        log.info('IP from coordinate limit: {}'.format(ip))
 
         time.sleep(0.1)
 
