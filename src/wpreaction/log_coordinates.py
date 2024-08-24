6a24f8aa4b51e58e5584ca8b9350710d0cd4272a
neko <neko@127.0.0.1>
2024-08-24 03:51:23 -0600

    try to fix log_coordinates

--- src/wpreaction/log_coordinates.py
+++ src/wpreaction/log_coordinates.py
@@ -1,4 +1,4 @@
-import logging, time
+import logging, time, ipaddress
 from geoip import geolite2
 
 
@@ -10,6 +10,13 @@ def log_coordinates(filename, north_limit, south_limit, east_limit, west_limit):
     while(True):
         line = file.readline()
         ip = line.split()[0]
+
+        try:
+            ipaddress.ip_address(str(ip))
+        except ValueError:
+            log.error('{} is not a valid IP!'.format(ip))
+            continue
+
         coordinates = geolite2.lookup(ip).location
 
         if(coordinates[0] <= north_limit and coordinates[0] >= south_limit and coordinates[1] >= eastlimit and coordinates[1] <= west_limit):
