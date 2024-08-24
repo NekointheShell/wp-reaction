82bf2f9b6a8695c04118f56d3f5f95941c3981bb
neko <neko@127.0.0.1>
2024-08-24 03:57:06 -0600

    forgot _

--- src/wpreaction/log_coordinates.py
+++ src/wpreaction/log_coordinates.py
@@ -21,7 +21,7 @@ def log_coordinates(filename, north_limit, south_limit, east_limit, west_limit):
         log.info('Looking up {}'.format(ip))
         coordinates = geolite2.lookup(ip).location
 
-        if(coordinates[0] <= north_limit and coordinates[0] >= south_limit and coordinates[1] >= eastlimit and coordinates[1] <= west_limit):
+        if(coordinates[0] <= north_limit and coordinates[0] >= south_limit and coordinates[1] >= east_limit and coordinates[1] <= west_limit):
             log.info('IP from coordinate limit: {}'.format(ip))
 
         time.sleep(1)
