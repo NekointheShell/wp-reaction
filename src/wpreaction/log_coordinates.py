34ca487504dbbfac9c51dda989758d73177eb0fd
neko <neko@127.0.0.1>
2024-08-24 04:27:15 -0600

    filter out blank lines in log_coordinates

--- src/wpreaction/log_coordinates.py
+++ src/wpreaction/log_coordinates.py
@@ -9,20 +9,22 @@ def log_coordinates(filename, north_limit, south_limit, east_limit, west_limit):
 
     while(True):
         line = file.readline()
-        ip = line.split()[0]
-        if(ip == '127.0.0.1'): continue
+        if(line != '' and line != "\n"):
+            ip = line.split()[0]
+            if(ip == '127.0.0.1'): continue
 
-        try:
-            ipaddress.ip_address(str(ip))
-        except ValueError:
-            log.error('{} is not a valid IP!'.format(ip))
-            continue
+            try:
+                ipaddress.ip_address(str(ip))
 
-        lookup = geolite2.lookup(ip)
-        if(lookup != None):
-            coordinates = lookup.location
+            except ValueError:
+                log.error('{} is not a valid IP!'.format(ip))
+                continue
 
-            if(coordinates[0] <= north_limit and coordinates[0] >= south_limit and coordinates[1] >= east_limit and coordinates[1] <= west_limit):
-                log.info('IP from coordinate limit: {}'.format(ip))
+            lookup = geolite2.lookup(ip)
+            if(lookup != None):
+                coordinates = lookup.location
+
+                if(coordinates[0] <= north_limit and coordinates[0] >= south_limit and coordinates[1] >= east_limit and coordinates[1] <= west_limit):
+                    log.info('IP from coordinate limit: {}'.format(ip))
 
     file.close()
