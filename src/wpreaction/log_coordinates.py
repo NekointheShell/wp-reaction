b9f2dbb4f4a77dd87350d447cec7b886da12d738
neko <neko@127.0.0.1>
2024-08-24 21:04:13 -0600

    another None check

--- src/wpreaction/log_coordinates.py
+++ src/wpreaction/log_coordinates.py
@@ -29,7 +29,7 @@ def log_coordinates(filename, north_limit, south_limit, east_limit, west_limit):
             if(lookup != None and hasattr(lookup, 'location')):
                 coordinates = lookup.location
 
-                if(len(coordinates) == 2):
+                if(coordinates != None and len(coordinates) == 2):
                     log.debug('log_coordinates: {} has coordinates {}'.format(ip, coordinates))
                     if(coordinates[0] <= north_limit and coordinates[0] >= south_limit and coordinates[1] <= east_limit and coordinates[1] >= west_limit):
                         log.info('log_coordinates: IP from coordinate limit: {}'.format(ip))
